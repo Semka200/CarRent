b10de7931a7f3ec3d6ce87ac553baa59a54631c9
syoma.gorin <syoma.gorin@gmail.com>
2026-03-29 18:40:55 +0300

    Добавил папку Sourse, заполнил MainWindow

--- CarRent/MainWindow.xaml.cs
+++ CarRent/MainWindow.xaml.cs
@@ -1,31 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
 
 namespace CarRent
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
     {
-        public MainWindow()
+        /// <summary>
+        /// Логика взаимодействия для MainWindow.xaml
+        /// </summary>
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
-            //Перенаправляем на страницу авторизации
-            FrameMain.Navigate(new Pages.LoginPage());
+            public MainWindow()
+            {
+                InitializeComponent();
+                //Перенаправляем на страницу авторизации
+                FrameMain.Navigate(new Pages.LoginPage());
+                // инициализируем обработку события ContentRendered сразу после загрузки страницы:
+                FrameMain.ContentRendered += new EventHandler(MainFrame_ContentRendered);
 
         }
+            //кнопка назад
+            private void BtnBack_Click(object sender, RoutedEventArgs e)
+            {
+                if (FrameMain.CanGoBack)
+                    FrameMain.GoBack();
+            }
+            //Скрываем кнопку назад на первой странице
+            private void MainFrame_ContentRendered(object sender, EventArgs e)
+            {
+                if (!FrameMain.CanGoBack || this.FrameMain.Content.GetType() == typeof(Pages.LoginPage))
+                {
+                    BtnBack.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    BtnBack.Visibility = Visibility.Visible;
+                }
+            }
+        }
     }
-}
